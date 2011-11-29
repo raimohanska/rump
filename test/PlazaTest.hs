4430e1ef957b192d3e411d8c0e00c5d29da83c12
Juha Paananen <juha.paananen@reaktor.fi>
2011-11-30 00:10:51 +0200

    fix tests

--- test/PlazaTest.hs
+++ test/PlazaTest.hs
@@ -4,31 +4,39 @@ import Test.HUnit
 import Plaza
 import Control.Concurrent.STM.TVar
 import Control.Concurrent.STM
+import Control.Monad
 import RumpInfo
 import GeoLocation
-
 
 -- TODO: instead of poking at internals, create a test-helper method:
 
 openMeetingsWithParticipants :: STM [[String]]
 openMeetingsWithParticipants = undefined
 
+testApp = "test-app"
+
 plazaTests = TestList [
   TestLabel "New meeting for new dude" $ TestCase $ do
-    (m, _) <- atomically $ lookupMeeting dude
-    meetingList <- readTVarIO currentMeetings
+    plaza <- newPlaza
+    add plaza dude
+    meetingList <- getMeetings plaza
     assertEqual "One meeting in list" 1 (length meetingList) 
   ,
   TestLabel "Second dude to same meeting" $ TestCase $ do
-    (m, _) <- atomically $ lookupMeeting dude
-    meetingList <- readTVarIO currentMeetings
+    plaza <- newPlaza
+    add plaza dude
+    add plaza dude
+    meetingList <- getMeetings plaza
     assertEqual "Still one meeting in list" 1 (length meetingList)
     participants <- readTVarIO $ participants $ head meetingList
     assertEqual "Two participants in Meeting" 2 (length participants)
   ,
   TestLabel "Distant dude to different meeting" $ TestCase $ do
-    (m, _) <- atomically $ lookupMeeting distantDude
-    meetingList <- readTVarIO currentMeetings
+    plaza <- newPlaza
+    add plaza dude
+    add plaza dude
+    add plaza distantDude
+    meetingList <- getMeetings plaza
     assertEqual "Two meetings in list" 2 (length meetingList)
     participants <- readTVarIO $ participants $ meetingList !! 1
     assertEqual "Two participants in original Meeting" 2 (length participants)
@@ -36,3 +44,6 @@ plazaTests = TestList [
 
 dude = RumpInfo "user" "The User" $ GeoLoc 0 0
 distantDude = RumpInfo "user" "The User" $ GeoLoc 100 100
+
+add plaza dude = void $ atomically $ lookupMeeting plaza testApp dude
+getMeetings plaza = readTVarIO $ currentMeetings plaza
