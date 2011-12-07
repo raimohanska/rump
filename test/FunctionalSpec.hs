6d0a2bc8058915ba35632c90dc75e20a89aec930
Juha Paananen <juha.paananen@reaktor.fi>
2011-12-07 23:59:47 +0200

    cleanup

--- test/FunctionalSpec.hs
+++ test/FunctionalSpec.hs
@@ -7,16 +7,16 @@ import Util.HttpTester
 import Util.ParallelTest
 import Util.TestWrapper
 
-exampleRequest = "{ \"userId\" : \"john\", \"displayName\" : \"John Kennedy\", \"location\": { \"latitude\": 51.0, \"longitude\": -0.1}}"
-exampleRequest2 = "{ \"userId\" : \"jack\", \"displayName\" : \"Jack Kennedy\", \"location\": { \"latitude\": 51.0, \"longitude\": -0.1}}"
-exampleResponse = "[{\"userId\":\"john\",\"displayName\":\"John Kennedy\",\"location\":{\"latitude\":51,\"longitude\":-0.1}}]"
-responseWithTwoGuys = "[{\"userId\":\"jack\",\"displayName\":\"Jack Kennedy\",\"location\":{\"latitude\":51,\"longitude\":-0.1}},{\"userId\":\"john\",\"displayName\":\"John Kennedy\",\"location\":{\"latitude\":51,\"longitude\":-0.1}}]"
+john = "{\"userId\":\"john\",\"displayName\":\"John Kennedy\",\"location\":{\"latitude\":51,\"longitude\":-0.1}}"
+jack = "{\"userId\":\"jack\",\"displayName\":\"Jack Kennedy\",\"location\":{\"latitude\":51,\"longitude\":-0.1}}"
+justJohn = "[" ++ john ++ "]"
+responseWithTwoGuys = "[" ++ jack ++ "," ++ john ++ "]"
 
 functionalTests = wrapTest withTestServer $ TestList [
-  post "New meeting for new dude" url "/testing" exampleRequest $ Exactly exampleResponse
+  post "New meeting for new dude" url "/testing" john $ Exactly justJohn
   ,parallelTests "Similar requests to common meeting" [
-      post "Dude 1" url "/testing" exampleRequest $ Exactly responseWithTwoGuys
-      ,delayTest $ post "Dude 1" url "/testing" exampleRequest2 $ Exactly responseWithTwoGuys
+      post "Dude 1" url "/testing" john $ Exactly jackAndJohn
+      ,delayTest $ post "Dude 1" url "/testing" jack $ Exactly responseWithTwoGuys
     ]
   ]
 
